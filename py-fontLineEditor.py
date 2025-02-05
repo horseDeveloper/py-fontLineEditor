52919499e497add0200b273c62a340403a88ed24
horseDeveloper <119028087+horseDeveloper@users.noreply.github.com>
2025-02-05 18:49:09 +0100

    Update py-fontLineEditor.py

--- py-fontLineEditor.py
+++ py-fontLineEditor.py
@@ -1,6 +1,10 @@
-# Copyright
-    # Line finder and editor for big .ttx files.
+# # Copyright
+    # Python line finder and editor for big .ttx (and other) files.
     # Copyright (C) 2025 Nicolò Arrigo
+    # License: AGPL-3.0-only
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, version 3.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 # # FILE INFO # 
 # ## Script summary:
